83926083ee1db995a5a2b7462110e451d80556d9
gkholman <gkenholman+github@gmail.com>
2023-08-30 11:33:15 -0400

    Completed assembly/production files for 20230829-2350z release.

---
+++ burn/burn2pdf.bat
@@ -0,0 +1,76 @@
+echo Processing 'burn/13-9x24-collage-board-baseball-crane' Files remaining: 37
+inkscape burn/13-9x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/13-9x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/46-9x24-collage-board-baseball-crane' Files remaining: 36
+inkscape burn/46-9x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/46-9x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/47-9x24-collage-board-baseball-crane' Files remaining: 35
+inkscape burn/47-9x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/47-9x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/25-9x24-collage-board-baseball-crane' Files remaining: 34
+inkscape burn/25-9x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/25-9x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/2552-18x24-collage-board-baseball-crane' Files remaining: 33
+inkscape burn/2552-18x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/2552-18x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/1364-18x24-collage-board-baseball-crane' Files remaining: 32
+inkscape burn/1364-18x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/1364-18x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/1374-18x24-collage-board-baseball-crane' Files remaining: 31
+inkscape burn/1374-18x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/1374-18x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/134625-27x24-collage-board-baseball-crane' Files remaining: 30
+inkscape burn/134625-27x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/134625-27x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/134725-27x24-collage-board-baseball-crane' Files remaining: 29
+inkscape burn/134725-27x24-collage-board-baseball-crane.svg --export-type=pdf --export-filename=burn/134725-27x24-collage-board-baseball-crane.pdf
+echo Processing 'burn/L7-cut-200x248-board-baseball-crane' Files remaining: 28
+inkscape burn/L7-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L7-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L7-crop-9x12-board-baseball-crane' Files remaining: 27
+inkscape burn/L7-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L7-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L7-frame-9x12-board-baseball-crane' Files remaining: 26
+inkscape burn/L7-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L7-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L6-cut-200x248-board-baseball-crane' Files remaining: 25
+inkscape burn/L6-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L6-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L6-crop-9x12-board-baseball-crane' Files remaining: 24
+inkscape burn/L6-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L6-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L5-cut-200x248-board-baseball-crane' Files remaining: 23
+inkscape burn/L5-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L5-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L5-crop-9x12-board-baseball-crane' Files remaining: 22
+inkscape burn/L5-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L5-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L5-frame-9x12-board-baseball-crane' Files remaining: 21
+inkscape burn/L5-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L5-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L4-cut-200x248-board-baseball-crane' Files remaining: 20
+inkscape burn/L4-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L4-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L4-crop-9x12-board-baseball-crane' Files remaining: 19
+inkscape burn/L4-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L4-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L4-frame-9x12-board-baseball-crane' Files remaining: 18
+inkscape burn/L4-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L4-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L3-cut-200x248-board-baseball-crane' Files remaining: 17
+inkscape burn/L3-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L3-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L3-crop-9x12-board-baseball-crane' Files remaining: 16
+inkscape burn/L3-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L3-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L3-frame-9x12-board-baseball-crane' Files remaining: 15
+inkscape burn/L3-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L3-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L2-cut-200x248-board-baseball-crane' Files remaining: 14
+inkscape burn/L2-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L2-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L2-crop-9x12-board-baseball-crane' Files remaining: 13
+inkscape burn/L2-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L2-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L2-frame-9x12-board-baseball-crane' Files remaining: 12
+inkscape burn/L2-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L2-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L1-cut-200x248-board-baseball-crane' Files remaining: 11
+inkscape burn/L1-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L1-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/L1-crop-9x12-board-baseball-crane' Files remaining: 10
+inkscape burn/L1-crop-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L1-crop-9x12-board-baseball-crane.pdf
+echo Processing 'burn/L1-frame-9x12-board-baseball-crane' Files remaining: 9
+inkscape burn/L1-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/L1-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/LT-frame-9x12-board-baseball-crane' Files remaining: 8
+inkscape burn/LT-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/LT-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/SBB-cut-200x248-board-baseball-crane' Files remaining: 7
+inkscape burn/SBB-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBB-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/SBB-frame-9x12-board-baseball-crane' Files remaining: 6
+inkscape burn/SBB-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBB-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/SBB-print-board-baseball-crane' Files remaining: 5
+inkscape burn/SBB-print-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBB-print-board-baseball-crane.pdf
+echo Processing 'burn/SBF-cut-200x248-board-baseball-crane' Files remaining: 4
+inkscape burn/SBF-cut-200x248-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBF-cut-200x248-board-baseball-crane.pdf
+echo Processing 'burn/SBF-frame-9x12-board-baseball-crane' Files remaining: 3
+inkscape burn/SBF-frame-9x12-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBF-frame-9x12-board-baseball-crane.pdf
+echo Processing 'burn/SBF-print-extended-board-baseball-crane' Files remaining: 2
+inkscape burn/SBF-print-extended-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBF-print-extended-board-baseball-crane.pdf
+echo Processing 'burn/SBF-print-basic-board-baseball-crane' Files remaining: 1
+inkscape burn/SBF-print-basic-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBF-print-basic-board-baseball-crane.pdf
+echo Processing 'burn/SBF-print-combo-board-baseball-crane' Files remaining: 0
+inkscape burn/SBF-print-combo-board-baseball-crane.svg --export-type=pdf --export-filename=burn/SBF-print-combo-board-baseball-crane.pdf
