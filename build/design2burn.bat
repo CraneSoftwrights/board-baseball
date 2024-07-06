41915f3cef6a90dc51655a3fc38dbfda245d063b
gkholman <gkenholman+github@gmail.com>
2024-07-05 21:34:31 -0400

    Add a make-all to make all of the outputs in an automated manner

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -1,9 +1,5 @@
-pushd burn
-if exist svg2pdf.bat del svg2svgpdfpng.bat
-if exist svg\*.svg del svg\*.svg
-if exist svg\*.svg.txt del svg\*.svg.txt
-if exist png\*.png del png\*.png
-if exist pdf\*.pdf del pdf\*.pdf
+rem @echo off
+pushd "%~dp0\..\burn"
 java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../build/design2burn.xsl -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
--- build/design2burn.sh
+++ build/design2burn.sh
@@ -1,10 +1,7 @@
-cd burn
-if [ -f svg2svgpdfpng.sh ]; then rm svg2svgpdfpng.sh ; fi
-find svg -name \*.svg -exec rm {} \;
-find svg -name \*.svg.txt -exec rm {} \;
-find png -name \*.png -exec rm {} \;
-find pdf -name \*.pdf -exec rm {} \;
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../build/design2burn.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane
+DP0=$( cd "$(dirname "$0")" ; pwd -P )
+pushd "$DP0/../burn" >/dev/null
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../build/design2burn.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane 
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 if [ -f svg2svgpdfpng.sh ]; then sh svg2svgpdfpng.sh ; fi
+popd >/dev/null
