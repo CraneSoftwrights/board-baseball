9a9b15873331086f2273e47f965db2131330faf6
gkholman <gkenholman+github@gmail.com>
2024-09-02 21:33:01 -0400

    Convert to using latest designSVG2burnFiles code

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -1,6 +1,6 @@
 rem @echo off
 pushd "%~dp0\..\burn"
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../utilities/designSVG2burnPDF/designSVG2burnPDF.xsl -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
 if exist svg2svgpdfpng.bat call svg2svgpdfpng.bat
--- build/design2burn.sh
+++ build/design2burn.sh
@@ -1,6 +1,6 @@
 DP0=$( cd "$(dirname "$0")" ; pwd -P )
 pushd "$DP0/../burn" >/dev/null
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../utilities/designSVG2burnPDF/designSVG2burnPDF.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane 
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane 
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 if [ -f svg2svgpdfpng.sh ]; then sh svg2svgpdfpng.sh ; fi
