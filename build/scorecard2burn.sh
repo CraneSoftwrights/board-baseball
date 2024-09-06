47de298d8065746ee9b8ab99b2ded48a1c5d468d
gkholman <gkenholman+github@gmail.com>
2024-09-06 13:37:52 -0400

    Remove base lines and add inning number to scorecard

--- build/scorecard2burn.sh
+++ build/scorecard2burn.sh
@@ -4,4 +4,9 @@ pushd "$DP0/../burn" >/dev/null
 
 inkscape "../design/scorecard-design-crane.svg" --actions-file="../build/scorecard-board-baseball-crane.svg.txt"
 
+if [ -f ../burn/png/scorecard-design-crane.png ]
+then
+cp ../burn/png/scorecard-design-crane.png ../shared/scorecard-design-crane.png
+fi 
+
 popd >/dev/null
