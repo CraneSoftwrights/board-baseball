f3bdc2c007ce066321b327107c5f012d7b5c4dcb
gkholman <gkenholman+github@gmail.com>
2024-09-06 13:59:01 -0400

    Clear base fills as only white fills on the artwork

--- build/scorecard2burn.sh
+++ build/scorecard2burn.sh
@@ -4,9 +4,9 @@ pushd "$DP0/../burn" >/dev/null
 
 inkscape "../design/scorecard-design-crane.svg" --actions-file="../build/scorecard-board-baseball-crane.svg.txt"
 
-if [ -f ../burn/png/scorecard-design-crane.png ]
+if [ -f png/scorecard-board-baseball-crane.png ]
 then
-cp ../burn/png/scorecard-design-crane.png ../shared/scorecard-design-crane.png
+cp png/scorecard-board-baseball-crane.png ../shared/scorecard-board-baseball-crane.png
 fi 
 
 popd >/dev/null
