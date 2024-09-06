e1ea4643446b29d72425851043de49904d5e10c6
gkholman <gkenholman+github@gmail.com>
2024-09-06 14:17:51 -0400

    Copy correct files into shared after having created subdirectories

--- make-all.sh
+++ make-all.sh
@@ -15,10 +15,10 @@ bash build/design2burn.sh 2>&1 | grep -v org.inkscape | grep -v "^\s*$"
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 
-cp burn/png/PBF-print-combo-board-baseball-crane.png shared/combo-board-baseball-crane.png 
-cp burn/png/PBF-print-basic-board-baseball-crane.png shared/basic-board-baseball-crane.png 
-cp burn/png/PBF-print-extended-board-baseball-crane.png shared/extended-board-baseball-crane.png 
-cp burn/png/PBB-print-board-baseball-crane.png shared/back-board-baseball-crane.png 
+cp burn/png/print/PBF-print-combo-board-baseball-crane.png shared/combo-board-baseball-crane.png 
+cp burn/png/print/PBF-print-basic-board-baseball-crane.png shared/basic-board-baseball-crane.png 
+cp burn/png/print/PBF-print-extended-board-baseball-crane.png shared/extended-board-baseball-crane.png 
+cp burn/png/print/PBB-print-board-baseball-crane.png shared/back-board-baseball-crane.png 
 cp burn/png/scorecard-board-baseball-crane.png shared/scorecard-board-baseball-crane.png 
 
 popd >/dev/null
