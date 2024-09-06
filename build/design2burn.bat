787e3228fc702f2c8754fa16bb04d58bdcbb7319
gkholman <gkenholman+github@gmail.com>
2024-09-06 16:39:32 -0400

    New required arguments to stylesheet

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -5,7 +5,7 @@ set ss=stylesheet-not-found-designSVG2burnFiles.xsl
 if exist ../../designSVG2burnFiles/designSVG2burnFiles.xsl ] set ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl
 if exist ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ] set ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl
 
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane minimum-stroke-width=.003in minimum-stroke-width=.003in cut-colour=ff00ff
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
 if exist svg2svgpdfpng.bat call svg2svgpdfpng.bat
--- build/design2burn.sh
+++ build/design2burn.sh
@@ -5,7 +5,7 @@ ss=stylesheet-not-found-designSVG2burnFiles.xsl
 if [ -f ../../designSVG2burnFiles/designSVG2burnFiles.xsl ]; then ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl ; fi
 if [ -f ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ]; then ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ; fi
 
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:$ss -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane 
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:$ss -o:svg2svgpdfpng.sh path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane minimum-stroke-width=.003in cut-colour=ff00ff
 errorReturn=$?
 if [ $errorReturn -ne 0 ]; then exit $errorReturn ; fi
 if [ -f svg2svgpdfpng.sh ]; then sh svg2svgpdfpng.sh ; fi
