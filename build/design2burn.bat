800c5faf08f89ed7f5c6d44f12fea06804f79bb2
gkholman <gkenholman+github@gmail.com>
2024-09-06 22:27:15 -0400

    New invocation arguments for design2burn stylesheet

--- build/design2burn.bat
+++ build/design2burn.bat
@@ -5,7 +5,7 @@ set ss=stylesheet-not-found-designSVG2burnFiles.xsl
 if exist ../../designSVG2burnFiles/designSVG2burnFiles.xsl ] set ss=../../designSVG2burnFiles/designSVG2burnFiles.xsl
 if exist ../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl ] set ss=../utilities/designSVG2burnFiles/designSVG2burnFiles.xsl
 
-java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane minimum-stroke-width=.003in minimum-stroke-width=.003in cut-colour=ff00ff
+java -jar ../utilities/saxon9he/saxon9he.jar -s:../design/design-board-baseball-crane.svg -xsl:%ss% -o:svg2svgpdfpng.bat path2svg=svg/ path2png=png/ path2pdf=pdf/ name-suffix=-board-baseball-crane minimum-stroke-width=.003in minimum-stroke-width=.003in cut-colour=ff00ff cut-width=.001in
 set errorReturn=%ERRORLEVEL%
 if %errorReturn% neq 0 exit /b %errorReturn%
 if exist svg2svgpdfpng.bat call svg2svgpdfpng.bat
